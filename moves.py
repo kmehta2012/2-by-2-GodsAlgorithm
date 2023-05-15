796f655c1dadc780312390e3822e93a7c0a352ea
Karan Mehta <kmehta2012@icloud.com>
2023-05-16 04:53:51 +0530

    basic outline of the moves set-up

---
+++ moves.py
@@ -0,0 +1,51 @@
+"""
+This script is an implementation of all the legal moves that can be done on a 3 * 3 * 3 cube.
+The standard singmaster notation will be used to describe the moves. 
+
+I'm going to assume that the cube is oriented white side up with the orange side on the left of white and red on the right.
+
+Standard moves that I'm going to implement:
+F (front), B (back), R (right), L (left), D (down), U (up), M, E, S 
+and their opposite directoins
+
+Standard moves that I'm going to ignore:
+1. Changing the orientation of the cube : x,y,z (I'm assuming a fixed orientation)
+2. Wide moves : Turning two slices at the same time (will be implemented as one slice at a time)
+
+"""
+
+def front(cube):
+    None
+
+def frontReverse():
+    None
+
+def back():
+    None
+
+def backReverse():
+    None
+
+def right():
+    None
+
+def rightReverse():
+    None
+
+def left():
+    None
+
+def leftReverse():
+    None
+
+def down():
+    None
+
+def downReverse():
+    None
+
+def up():
+    None
+
+def upReverse():
+    None
