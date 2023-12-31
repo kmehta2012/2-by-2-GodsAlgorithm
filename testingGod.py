25fa29f16382efba7a774594a8c35a0523a7e552
Karan Mehta <kmehta2012@icloud.com>
2023-12-31 16:41:02 -0500

    I did itgit add testingGod.py git add testingGod.py git add testingGod.py  initial -> solved bfs works. Just need to convert to 2-way and then I'm done.

--- testingGod.py
+++ testingGod.py
@@ -1,6 +1,8 @@
-from GodsAlgorithm import *
+import GodsAlgorithm as ga
+from Cube import *
 #The following is just 2 quarter twists away from a solved configuration (L' L')
 # I'm gonna print out all all reachable configurations in 2 quarter twists (via BFS) to see if my implementation indeed works or not
+"""
 initialState = ('w', 'g', 'o', 
                 'w', 'b', 'o',
                 'y', 'b', 'r',
@@ -8,7 +10,16 @@ initialState = ('w', 'g', 'o',
                 'y', 'g', 'o',
                 'y', 'b', 'o',
                 'w', 'b', 'r',
-                'w', 'g', 'r')
+                'w', 'g', 'r') 
+                """
+initialState = ('w', 'r', 'g',
+                'w', 'o', 'g',
+                'w', 'r', 'b',
+                'w', 'o', 'b',
+                'y', 'r', 'g',
+                'y', 'o', 'g',
+                'y', 'r', 'b',
+                'y', 'o', 'b') #Solved config - for testing purposes
 
 solvedState = None #NEED TO FIGURE THIS SHIT OUT
 """
@@ -22,16 +33,24 @@ This parameter is arguably not needed - but it doesn't hurt to have it. Besides 
 initialBfsNode = initialState #Represents the current node in the BFS queue that we're exploring (initial -> solved direcion)
 solvedBfsNode = solvedState  #Represents the current node in the BFS queue that we're exploring (solved -> initial direction)
 
-adjList = {initialState : [None, None, moveNumber]} # FORMAT - { State : [predecessor, Move that gets me from predecessor to State, moveNumber] }
-configGraph = Graph(adjList) #Also acts as BFS queue in the initial -> solved direction
+adjList = {initialState : [None, None, ga.moveNumber]} # FORMAT - { State : [predecessor, Move that gets me from predecessor to State, moveNumber] }
+ga.bfsQueue.append(initialState)
+
+
+configGraph = ga.Graph(adjList) #Also acts as BFS queue in the initial -> solved direction
 pocketCube = Cube(initialState)
 
-for key, value in list(configGraph.adjacency_list.items()):
+
+while ga.bfsQueue:
+    
+    key = ga.bfsQueue.pop(0)
     pocketCube.state = key
-    moveNumber = value[2] + 1
-    if(moveNumber == 3):
+    ga.moveNumber = configGraph.adjacency_list[key][2] + 1
+    print(ga.moveNumber)
+    if(ga.moveNumber == 15):
         break
     configGraph.add_neighbours(pocketCube)
 
-print(moveNumber)
-printAdjList(configGraph.adjacency_list)
+#print(moveNumber)
+#ga.printAdjList(configGraph.adjacency_list)
+print(f"Number of configurations : {len(configGraph.adjacency_list)}")
